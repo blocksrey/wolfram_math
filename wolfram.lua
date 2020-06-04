92f245dfdf367b814c87ed0cf6227c79ff239def
Jeffrey Skinner <17016344+blocksrocks@users.noreply.github.com>
2020-06-04 02:53:52 -0700

    removed lite because it requires license

--- wolfram.lua
+++ wolfram.lua
@@ -4,10 +4,9 @@ local sub   = string.sub
 local handle = popen("cd")
 local output = handle:read("*l")
 handle:close()
-local directory = output.."/wolframlite/math.exe"
 
 local function wolfram(wlstr)
-	local handle = popen(directory..' -noprompt -run "Print[ToString[InputForm['..wlstr..']]] Exit[]"', "r")
+	local handle = popen('math -noprompt -run "Print[ToString[InputForm['..wlstr..']]] Exit[]"', "r")
 	local output = handle:read("*l")
 	handle:close()
 	return sub(output, 2, -2)
